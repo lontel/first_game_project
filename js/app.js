f2467ead309f659429c5ace2d3afe8b4e25c8150
lontel <lonte.leon@gmail.com>
2022-06-12 17:01:09 +0200

    second commit

--- js/app.js
+++ js/app.js
@@ -5,11 +5,13 @@ const Game = {
     version: '1.0.0',
     canvasDom: undefined,
     ctx: undefined,
+    FPS: 60,
     keys: {
         jump: 'ArrowUp',
         moveLeft: 'ArrowLeft',
         moveRight: 'ArrowRight',
-        shot: 'Space'
+        shot: 'Space',
+        moveDown: 'ArrowDown',
     },
     canvasSize: {
         w: undefined,
@@ -20,6 +22,8 @@ const Game = {
         this.ctx = this.canvasDom.getContext('2d')
         this.setDimensions()
         this.setEventListeners()
+        this.start()
+        this.createAll()
     },
     setDimensions() {
         this.canvasSize.w = window.innerWidth
@@ -27,17 +31,37 @@ const Game = {
         this.canvasDom.setAttribute('width', this.canvasSize.w)
         this.canvasDom.setAttribute('height', this.canvasSize.h)
     },
-    start() {
-        setInterval(() =>{
-            this.clearAll()
-            this.drawAll()
-        }, 50)
+    clearAll() {
+        this.ctx.clearRect(0, 0, this.canvasSize.w, this.canvasSize.h)
     },
-    clearAll(){
+    createAll() {
+        this.player = new Player(this.ctx, 500, 500, 100, 100)
+        
 
     },
-    drawAll(){
-        
+    drawAll() {
+        this.player.createPlayer()
+    },
+    setEventListeners() {
+        document.onkeydown = event => {
+            switch (event.code) {
+                case this.keys.jump: this.player.jump()
+                    break;
+                case this.keys.moveLeft: this.player.moveLeft()
+                    break;
+                case this.keys.moveRight: this.player.moveRight()
+                    break;
+                case this.keys.shot: this.player.shot()
+                    break;
+                case this.keys.moveDown: this.player.moveDown()
+                    break;
+            }
+        }
+    },
+    start() {
+        setInterval(() => {
+            this.clearAll()
+            this.drawAll()
+        }, 1000 / this.FPS)
     }
-    
 }
--- js/player.js
+++ js/player.js
@@ -1,49 +1,54 @@
 class Player {
-    constructor(ctx, canvasSize) {
+    constructor(ctx, playerPosX, playerPosY, playerSizeW, playerSizeH) {
         this.ctx = ctx
-        this.playerSize = { w: 100, h: 100 }
         this.playerPos = {
-            x: this.playerSize.w + 30,
-            y: canvasSize.h - this.playerSize.h - 30
+            x: playerPosX,
+            y: playerPosY
         }
-        this.playerSpeed = { x: 10, y: 0 }
-        this.canvasSize = canvasSize
+        this.playerSize = {
+            w: playerSizeW,
+            h: playerSizeH
+        }
+        this.playerSpeed = { x: 50, y: 0 }
         this.physics = { gravity: 0.4 }
-        this.playerImage = './img/player.jpeg'
-        this.imageInstance = undefined
+        // this.canvasSize = {
+        //     w: canvasSizeW,
+        //     h: canvasSizeH 
+        // }
 
         this.init()
     }
     init() {
-        this.imageInstance = new Image()
-        this.imageInstance.src = this.playerImage
+        this.jump()
+        this.moveLeft()
+        this.moveRight()
+        this.shot()
+        this.createPlayer()
+        this.moveDown()
     }
-    setEventListeners() {
-        document.onkeydown = event => {
-            switch (event.code) {
-                case this.keys.jump:
-                    if (this.playerPos.x) {
-                        this.jump()
-                    }
-                    break;
-                case this.keys.moveLeft: this.moveLeft()
-                    break;
-                case this.keys.moveRight: this.moveRight()
-                    break;
-                case this.keys.shot: this.shot()
-                    break;
-            }
-
-        }
+    createPlayer() {
+        this.ctx.fillStyle = 'blue'
+        this.ctx.fillRect(this.playerPos.x, this.playerPos.y, this.playerSize.w, this.playerSize.h)
     }
     jump() {
         this.playerPos.y -= 40;
-        this.playerSpeed.y -= 8
+        this.playerSpeed.y += 8
+        // if (this.playerPos.y === window.innerHeight / 1.3) {    //no funciona, revisar
+            
+        // }
     }
     moveLeft() {
         this.playerPos.x -= 20
     }
     moveRight() {
         this.playerPos.x += 20
+    }
+    moveDown() {
+        if (this.playerPos.y < window.innerHeight - this.playerSize.h - 50) {
+            this.playerPos.y += 40
+        }
+    }
+    shot() {
+
     }
 }
